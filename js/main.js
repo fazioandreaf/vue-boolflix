7fda9b53b6caeb5714b6928074b9dd5650f184e0
fazioandrea.f <fazioandrea.f@gmail.com>
2021-04-19 11:50:55 +0200

    .

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
-init(){
-    console.log('ciao');
+function init(){
+    new Vue({
+        el:'#app',
+        data:{
+            query:'batman',
+            prova:''
+        },
+        mounted(){
+            axios.get('https://api.themoviedb.org/3/search/movie', {
+                params: {
+                    'api_key': '2087fd848b765980c5bf5832959724ef',
+                    'query':  this.query,
+                }
+            })
+         .then(data => {
+             this.prova=data.data.results[0].original_title;
+             console.log(this.prova)
+            })
+         .catch(()=>
+         console.log('error'));
+        },
+    })
 }
 document.addEventListener('DOMContentLoader',init())
