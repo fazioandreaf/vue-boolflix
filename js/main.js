39bee0aeb98d85905bf3b8baae439cc3a749d937
fazioandrea.f <fazioandrea.f@gmail.com>
2021-04-19 13:07:05 +0200

    1 mileston fatta

--- js/main.js
+++ js/main.js
@@ -2,23 +2,33 @@ function init(){
     new Vue({
         el:'#app',
         data:{
-            query:'batman',
+            query:'',
             allResults:''
         },
         mounted(){
-            axios.get('https://api.themoviedb.org/3/search/movie', {
-                params: {
-                    'api_key': '2087fd848b765980c5bf5832959724ef',
-                    'query':  this.query,
-                }
-            })
-         .then(data => {
-             this.allResults=data.data;
-             console.log(this.allResults)
-            })
-         .catch(()=>
-         console.log('error'));
+            this.callAPI(this.query)
         },
+        'methods':{
+            callAPI:function(elem){
+                axios.get('https://api.themoviedb.org/3/search/movie', {
+                    params: {
+                        'api_key': '2087fd848b765980c5bf5832959724ef',
+                        'query':  elem,
+                    }
+                })
+             .then(data => {
+                 this.allResults=data.data;
+                 console.log(this.allResults)
+                })
+             .catch(()=>
+             console.log('error'))
+
+            },
+            addQuery:function(){
+                this.callAPI(this.query)
+                console.log(this.query);
+            }
+        }
     })
 }
 document.addEventListener('DOMContentLoaded',init);
